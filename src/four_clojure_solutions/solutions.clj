d2d9cc557374ac548a83156f5eed4364198aae0a
KucksE <kuckse@pragmatics.com>
2015-03-10 18:36:06 -0400

    more solutions

--- src/four_clojure_solutions/solutions.clj
+++ src/four_clojure_solutions/solutions.clj
@@ -1,4 +1,4 @@
-(ns four-clojure-solutions.infix-calculator)
+(ns four-clojure-solutions.solutions)
 
 (defn infix [x & args]
   (reduce 
@@ -18,4 +18,25 @@
                               (for [[k v] (partition 2 (interleave (map f s) s))]
                                 {k [v]}))]
     (apply merge-with (cons into (get-singleton-maps f s)))))
-  
+
+; pascal's triangle
+
+(defn get-pascal-row [row]
+  (letfn [(calculate-row [prevRow]
+                         (flatten [1 
+                                   (map (fn [[x y]] (+ x y)) (partition 2 1 prevRow)) 
+                                   1]))]
+    (nth (iterate calculate-row [1]) (dec row)))) 
+
+(defn disjoint? [sets]
+  (= (apply + (map count sets)) 
+     (count (reduce clojure.set/union sets)))
+  ) 
+
+(defn symmetric-tree? [tree] 
+  (letfn [(reverse-tree [[parent left right]] 
+    [parent
+     (if (coll? right) (reverse-tree right) right)
+		 (if (coll? left) (reverse-tree left) left)])]
+     (= (second tree)
+		(reverse-tree (nth tree 2)))))
