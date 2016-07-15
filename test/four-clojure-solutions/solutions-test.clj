fc868bddeecf21dbf53031a0c79342b128632895
Eric Kucks <kuckse@pragmatics.com>
2016-07-15 16:30:32 -0400

    adding seq-pronunciations test

--- test/four-clojure-solutions/solutions-test.clj
+++ test/four-clojure-solutions/solutions-test.clj
@@ -10,3 +10,7 @@
   (is (= (keys-and-values [:a 1]) {:a [1]}))
   (is (= (keys-and-values [:a 1, :b 2]) {:a [1], :b [2]}))
   (is (= (keys-and-values [:a 1 2 3, :b :c 4]) {:a [1 2 3], :b [], :c [4]})))
+
+(deftest seq-pronunciations-test
+  (is (= (take 3 (seq-pronunciations [1])) [[1 1] [2 1] [1 2 1 1]]))
+  (is (= (first (seq-pronunciations [1 1 1 4 4])) [3 1 2 4])))
