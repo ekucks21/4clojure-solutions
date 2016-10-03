21ce3860374bbba39ef5641865871c7174561aac
Eric Kucks <kuckse@pragmatics.com>
2016-10-03 17:50:40 -0400

    more tests for #103

--- test/four-clojure-solutions/solutions-test.clj
+++ test/four-clojure-solutions/solutions-test.clj
@@ -49,5 +49,7 @@
 
 (deftest k-combinations-test
   (is (= (k-combinations 1 #{4 5 6}) #{#{4} #{5} #{6}}))
+  (is (= (k-combinations 4 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a "abc" "efg"}}))
+  (is (= (k-combinations 2 #{[1 2 3] :a "abc" "efg"}) #{#{[1 2 3] :a} #{[1 2 3] "abc"} #{[1 2 3] "efg"} #{:a "abc"} #{:a "efg"} #{"abc" "efg"}}))
   (is (= (k-combinations 3 #{0 1 2 3 4}) #{#{0 1 2} #{0 1 3} #{0 1 4} #{0 2 3} #{0 2 4}
                                            #{0 3 4} #{1 2 3} #{1 2 4} #{1 3 4} #{2 3 4}})))
